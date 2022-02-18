c4ee35597b73e03a416cb8fe5b4eed67b70ebf68
zacway <59920489+zacway@users.noreply.github.com>
2022-02-18 15:43:53 +0100

    j'ai changé plein de trucs

--- traitement_video/video_tracking.py
+++ traitement_video/video_tracking.py
@@ -4,31 +4,42 @@ from moviepy.video.fx.all import scroll
 from moviepy.video.tools.tracking import manual_tracking
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
-clip = VideoFileClip("Les marcheurs, gouttes et ondes.mp4")
-clip = clip.subclip(17, 31)
+import matplotlib.pyplot as plt
+
+clip = VideoFileClip("20220211_175425.mp4")
+clip = clip.subclip(6.5, 8.5)
  
 
-
-
- 
 
 
 (w, h) = clip.size
-cropped_clip = crop(clip, width=w/2, height=h/2, x_center=w*3/4, y_center=h/2)
 
-# trajectory = manual_tracking(cropped_clip, t1=0, t2=31-17, fps=1,nobjects=1, savefile="track.txt")
-# ...
-# LATER, IN ANOTHER SCRIPT, RECOVER THESE TRAJECTORIES
+# cropping = manual_tracking(clip, t1=0, t2=1, fps=2,nobjects=1, savefile="cropping.txt")
+
 from moviepy.video.tools.tracking import Trajectory
-traj= Trajectory.load_list('track.txt')[0]
+cropping= Trajectory.load_list("cropping.txt")[0]
+croppingwidth=cropping.xx[1]-cropping.xx[0]
+croppingx=(cropping.xx[1]+cropping.xx[0])/2
 
-speed=(traj.xx[-1]-traj.xx[0])/traj.tt[-1]
-new_clip = scroll(cropped_clip, w=clip.w//20, h=clip.h//20  , x_speed=speed,x_start=traj.xx[0], y_start=traj.yy[0])
+croppingy=(cropping.yy[1]+cropping.yy[0])/2
+croppingheight=cropping.yy[1]-cropping.yy[0]
+
+
+cropped_clip = crop(clip, width=croppingwidth, height=h/2, x_center=croppingx, y_center=croppingy)
+
+
+# speed=(traj.xx[-1]-traj.xx[0])/traj.tt[-1]
+# new_clip = scroll(cropped_clip, w=clip.w//20, h=clip.h//20  , x_speed=speed,x_start=traj.xx[0], y_start=traj.yy[0])
+
+# track = manual_tracking(cropped_clip, t1=0, t2=4, fps=15,nobjects=1, savefile="tracking2.txt")
 
 
 
 
-new_clip.write_videofile('goutte.mp4')
+tracking= Trajectory.load_list("tracking2.txt")[0]
+plt.plot(tracking.tt,tracking.yy)
+
+# cropped_clip.write_videofile('goutte.mp4')
 
 
 
