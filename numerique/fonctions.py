1eb8ca15c433a44b8584b9de15f3c37d405393b5
Adrien Anthore <75272960+ADnothing@users.noreply.github.com>
2022-04-08 16:35:36 +0200

    updates
    Commentaire et amélioration des plots + taille moyenne d'une bille

--- numerique/fonctions.py
+++ numerique/fonctions.py
@@ -1,8 +1,12 @@
 from lib import *
 
+#Calibration des mesures
+#Donne l'accélération du plateau à partir 
+#de l'intensité du courrant électrique de l'accéléromètre
 def convert(A):
     return (A*2*9.81)/380
 
+#récupère les donnés du diagramme des phases depuis le csv "DATA.csv"
 def get_datas():
     
     datas = pd.read_csv('DATA.csv')
@@ -49,57 +53,71 @@ def get_datas():
     
     return (F, A1, A1err, A2, A2err, seuil_far, seuil_farerr, seuil_co, seuil_coerr, march, marcherr)
 
+#Loi théorique trouvé par analyse dimentionnelle du seuil de l'instabilité de
+#faraday en fonction de la fréquence
 def loi(f, sigma, v):
     return (2**(4/3))*((950/sigma)**(1/3))*v*(2*np.pi*f)**(5/3)
 
+#Plot chaques étapes de la réalisation du diagramme des phases
 def plot_all(F, A1, A1err, A2, A2err, seuil_far, seuil_farerr, seuil_co, seuil_coerr, march, marcherr, Y, th):
     
-    plt.errorbar(F, A1, yerr=A1err, fmt='o', label='Mesure 1')
-    plt.errorbar(F, A2, yerr=A2err, fmt='o', label='Mesure 2')
+    #Plot des deux mesures de l'instabilité de faraday
+    plt.errorbar(F, A1, fmt='-o', label='Mesure 1')
+    plt.errorbar(F, A2, fmt='-o', label='Mesure 2')
+    plt.fill_between(F, A1-A1err, A1+A1err, alpha=0.75)
+    plt.fill_between(F, A2-A2err, A2+A2err, alpha=0.75)
     plt.xlabel("Fréquence (Hz)")
     plt.ylabel("Accélération ($m.s^{2}$)")
     plt.legend(bbox_to_anchor=(1.1, 0.75), loc='upper left', borderaxespad=0.)
     plt.show()
     
-    plt.errorbar(F, A1, yerr=A1err, fmt='o', label='Mesure 1')
-    plt.errorbar(F, A2, yerr=A2err, fmt='o', label='Mesure 2')
-    plt.errorbar(F, seuil_far, yerr=seuil_farerr, fmt='o', label="Seuil d'instabilité")
+    #Plot comparatif de la moyenne des instabilités de faraday avec incertitudes
+    plt.errorbar(F, A1, fmt='-o', label='Mesure 1')
+    plt.errorbar(F, A2, fmt='-o', label='Mesure 2')
+    plt.errorbar(F, seuil_far, fmt='-o', label="Seuil d'instabilité")
+    plt.fill_between(F, A1-A1err, A1+A1err, alpha=0.75)
+    plt.fill_between(F, A2-A2err, A2+A2err, alpha=0.75)
+    plt.fill_between(F, seuil_far-seuil_farerr, seuil_far+seuil_farerr, alpha=0.75)
     plt.xlabel("Fréquence (Hz)")
     plt.ylabel("Accélération ($m.s^{2}$)")
     plt.legend(bbox_to_anchor=(1.1, 0.75), loc='upper left', borderaxespad=0.)
     plt.show()
     
-    plt.errorbar(F, seuil_far, yerr=seuil_farerr, fmt='o', label="Seuil d'instabilité")
+    #Plot fit de l'isntabilité de faraday
+    plt.errorbar(F, seuil_far, fmt='-o', label="Seuil d'instabilité")
+    plt.fill_between(F, seuil_far-seuil_farerr, seuil_far+seuil_farerr, alpha=0.75)
     plt.plot(F, Y, label='fit')
     plt.xlabel("Fréquence (Hz)")
     plt.ylabel("Accélération ($m.s^{2}$)")
     plt.legend(bbox_to_anchor=(1.1, 0.75), loc='upper left', borderaxespad=0.)
     plt.show()
     
+    #comparasion fit et théorique
     plt.plot(F, th, label='Modèle Théorique')
-    plt.errorbar(F, seuil_far, yerr=seuil_farerr, fmt='o', label="Seuil d'instabilité")
+    plt.errorbar(F, seuil_far, fmt='-o', label="Seuil d'instabilité")
+    plt.fill_between(F, seuil_far-seuil_farerr, seuil_far+seuil_farerr, color='orange', alpha=0.75)
     plt.plot(F, Y, label='fit')
     plt.ylabel("Accélération ($m.s^{2}$)")
     plt.xlabel("Fréquence (Hz)")
     plt.legend(bbox_to_anchor=(1.1, 0.75), loc='upper left', borderaxespad=0.)
     plt.show()
     
-    plt.errorbar(F, seuil_co, yerr=seuil_coerr, fmt='-o', label='Seuil de coalescence')
-    plt.errorbar(F, seuil_far, yerr=seuil_farerr, fmt='-o', label="Seuil d'instabilité")
-    plt.errorbar(F, march, yerr=marcherr, fmt='-o', label="Seuil des marcheurs")
-    plt.ylabel("Accélération ($m.s^{2}$)")
-    plt.xlabel("Fréquence (Hz)")
-    plt.legend(bbox_to_anchor=(1.1, 0.75), loc='upper left', borderaxespad=0.)
-    plt.show()
     
-    plt.errorbar(F, seuil_co, yerr=seuil_coerr, fmt='-o', color='black')
-    plt.errorbar(F, seuil_far, yerr=seuil_farerr, fmt='-o', color='black')
-    plt.errorbar(F, march, yerr=marcherr, fmt='-o', color='black')
+    #Diagramme des phases avec régions colorés
+    plt.errorbar(F, seuil_co, fmt='-o', color='black')
+    plt.errorbar(F, seuil_far, fmt='-o', color='black')
+    plt.errorbar(F, march, fmt='-o', color='black')
+    
     plt.fill_between(F, seuil_far, march, color='springgreen')
     plt.fill_between(F[F<=45], seuil_far[F<=45], seuil_co[F<=45], color='gold')
     plt.fill_between(F[F>=45], march[F>=45], seuil_co[F>=45], color='gold')
     plt.fill_between(F, seuil_co, color='deepskyblue')
     plt.fill_between(F, seuil_far, np.max(seuil_far), color='lightcoral')
+    
+    plt.fill_between(F, seuil_co-seuil_coerr, seuil_co+seuil_coerr, alpha=0.75)
+    plt.fill_between(F, seuil_far-seuil_farerr, seuil_far+seuil_farerr, alpha=0.75)
+    plt.fill_between(F, march-marcherr, march+marcherr, alpha=0.75)
+    
     plt.text(25,20,'Instabilité de Faraday')
     plt.text(60, 17.5,'Marcheurs')
     plt.text(50,1,'Coalescence')
@@ -107,4 +125,42 @@ def plot_all(F, A1, A1err, A2, A2err, seuil_far, seuil_farerr, seuil_co, seuil_c
     plt.ylabel("Accélération ($m.s^{2}$)")
     plt.xlabel("Fréquence (Hz)")
     plt.show()
-
+    
+    #Diagramme des phases avec régions colorés + fit
+    plt.errorbar(F, seuil_co, fmt='-o', color='black')
+    plt.errorbar(F, seuil_far, fmt='-o', color='black')
+    plt.errorbar(F, march, fmt='-o', color='black')
+    plt.plot(F, Y, ls='--', color='white', lw=3,label='fit del\'instabilité\nde faraday')
+    
+    plt.fill_between(F, seuil_far, march, color='springgreen')
+    plt.fill_between(F[F<=45], seuil_far[F<=45], seuil_co[F<=45], color='gold')
+    plt.fill_between(F[F>=45], march[F>=45], seuil_co[F>=45], color='gold')
+    plt.fill_between(F, seuil_co, color='deepskyblue')
+    plt.fill_between(F, seuil_far, np.max(seuil_far), color='lightcoral')
+    
+    plt.fill_between(F, seuil_co-seuil_coerr, seuil_co+seuil_coerr, alpha=0.75)
+    plt.fill_between(F, seuil_far-seuil_farerr, seuil_far+seuil_farerr, alpha=0.75)
+    plt.fill_between(F, march-marcherr, march+marcherr, alpha=0.75)
+    
+    plt.text(25,20,'Instabilité de Faraday')
+    plt.text(60, 17.5,'Marcheurs')
+    plt.text(50,1,'Coalescence')
+    plt.text(45,8,'Rebonds')
+    plt.ylabel("Accélération ($m.s^{2}$)")
+    plt.xlabel("Fréquence (Hz)")
+    plt.legend(bbox_to_anchor=(1.05, 0.75), loc='upper left', borderaxespad=0., facecolor='black', labelcolor='white')
+    plt.show()
+    
+#Donne la taille moyenne des goutes avec incertitude
+def taille_gouttes(file_npz):
+        
+    Rg = file_npz.f.arr_0
+    Rg_err = file_npz.f.arr_1
+        
+    R = Rg.mean()
+    a = Rg_err.mean()
+    b = Rg.std()
+    c = Rg_err.std()
+    R_err = np.sqrt(a**2 + b**2 + c**2)
+        
+    print("La taille des petites gouttes est : {:.2} (+/-) {:.2}".format(R, R_err))
--- numerique/main.py
+++ numerique/main.py
@@ -3,15 +3,28 @@ from fonctions import *
 
 if __name__ == "__main__":
     
+    #Fichier npz qui contient les numpy arrays des mesures des tailles des gouttes
+    #avec leurs incertitudes
+    datas_R = np.load('R_gouttes.npz')
+    
+    #On récupère les paramètres mesurés pour le diagramme des phases
     F, A1, A1err, A2, A2err, seuil_far, seuil_farerr, seuil_co, seuil_coerr, march, marcherr = get_datas()
     
+    #Fit du seuil d'instabilité de faraday
     popt, pcov = curve_fit(loi, F, seuil_far)
     Y = loi(F, popt[0], popt[1])
     th = loi(F, 21e-3, 20e-6)
 
+    #Plots de toutes les étapes du diagramme des phases
     plot_all(F, A1, A1err, A2, A2err, seuil_far, seuil_farerr, seuil_co, seuil_coerr,march, marcherr, Y, th)
 
+    #Paramètres du fit
     print("sigma = {:.3} (+/-) {:.3}".format(popt[0], np.sqrt(pcov[0][0])))
     print("v = {:.3} (+/-) {:.3}".format(popt[1], np.sqrt(pcov[1][1])))
     print("erreur relative sigma : {:.4} %".format((popt[0]-21e-3)*100/21e-3))
     print("erreur relative v : {:.3} %".format(-(popt[1]-20e-6)*100/20e-6))
+    
+    print("=========================================================")
+    
+    #Taille moyenne des goutes "petites"
+    taille_gouttes(datas_R)
