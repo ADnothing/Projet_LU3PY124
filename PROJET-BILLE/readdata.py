3c03ef317fc6483ae7c336feae45e734a86d1f84
zacway <59920489+zacway@users.noreply.github.com>
2022-04-17 20:12:28 +0200

    read

---
+++ PROJET-BILLE/readdata.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 10 19:29:17 2022
+
+@author: Zacharie
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+f=np.load("freqx.npz")
+h=np.load("h.npz")
+f=f.f.arr_0
+h=h.f.arr_0
+
+# plt.xlim(0.0004714,0.0004716)
+plt.plot(f, h, ',',alpha=0.02)
+
+
+
+
+
+# hmean=np.load("hmean.npz")
+# dtmean=np.load("dtmean.npz")
+# hmean=hmean.f.arr_0
+# dtmean=dtmean.f.arr_0
+
+
+# plt.plot(dtmean[0:400], hmean[0:400], ',')
+# plt.plot(dtmean[0:400], hmean[0:400], ',')
+
+# plt.ylim(0.00115,0.0028)
+# plt.xlim(0.03125,0.0349)
+# plt.plot(dtmean[2000:], hmean[2000:], ',')
+# plt.plot(dtmean[:1000], hmean[:1000], ',')
